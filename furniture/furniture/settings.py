f1d8fffe8a528e08a2b624add26e4a9ad5ab8805
Sharad Shukla <you@example.com>
2025-06-10 18:13:22 +0530

    add some file

--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,10 @@ WSGI_APPLICATION = 'furniture.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3', # लोकल डेवलपमेंट के लिए
+        conn_max_age=600  # कनेक्शन को री-यूज़ करने में मदद करता है
+    )
 }
 
 
